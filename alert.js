ba515d9d5e799d38558585cd1df312b15d0a0625
Shah Wajahat <Shah Wajahat>
2016-09-20 00:44:12 -0700

    this is the file of jaa script topic alert and ariable

---
+++ alert.js
@@ -0,0 +1,4 @@
+alert("I am shah wajahat")
+alert("this my first practise of Javascript")
+
+alert("Thankyou for using my massage");
---
+++ var.js
@@ -0,0 +1,3 @@
+var name = "Shah Wajahat";
+name = "Waji";
+alert(name);
